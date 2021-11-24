84f143ef7eae6f4583c6bd86cb00b7ca40349db1
Kamila Topolska <kamiira97@gmail.com>
2021-11-24 18:26:26 +0100

    dobrze je e e e eeest

--- Astar.py
+++ Astar.py
@@ -1,6 +1,8 @@
 from functions import get_position
 from Node import Node
 import collections
+import operator
+import datetime
 
 
 class Astar:
@@ -21,7 +23,7 @@ class Astar:
 
     def compute_cost_g(self, node):
         cost = 0
-        while node:
+        while node.direction is not None:
             node = node.parent_node
             cost += 1
 
@@ -38,37 +40,67 @@ class Astar:
 
 
     def solve(self):
-        # parents_pool = []
         visited_boards = []
+        LRUD_astar_sequence = []
+        parents_pool = []
 
         visited_nodes_stats = 0
         processed_nodes_stats = 0
         moves_sequence = ['L', 'R', 'U', 'D']
 
+        start_time = datetime.datetime.now()
+
         root = Node(self.starting_board, None, None, None)
         root.distance = self.calculate_f_value(root)
         if root.board == self.goal_board:
             print('Solution found.')
+            end_time = datetime.datetime.now()
+            exec_time = (end_time - start_time).total_seconds() * 1000
 
-        # parents_pool.append(root)
-        parents_pool = collections.deque([root])
-        seen = set()
-        seen.add(self.state(parents_pool[0]))
+            depth = 0
+            solution_length = 0
+            LRUD_solution_sequence = None
+            return LRUD_solution_sequence, solution_length, depth, exec_time, visited_nodes_stats, processed_nodes_stats
+
+        visited_nodes_stats += 1
+        processed_nodes_stats += 1
+        visited_boards.append(root.board)
+
+        parents_pool.append(root)
 
         while parents_pool:
-            # parents_pool.sort(key=lambda x: x.distance)
-            # current_parent = parents_pool.pop(0)
+            parents_pool = sorted(parents_pool, key=operator.attrgetter('distance'))
 
-            collections.deque(sorted(list(parents_pool), key=lambda node: node.distance))
-            current_parent = parents_pool.popleft()
-
-            print("GÓWNO")
-            for parent in parents_pool:
-                print(str(parent) + "z chujowym dystansem: " + str(parent.distance))
+            current_parent = parents_pool.pop(0)
 
             if current_parent.board == self.goal_board:
-                print('Solfound')
-                return
+                end_time = datetime.datetime.now()
+                exec_time = (end_time - start_time).total_seconds() * 1000
+
+                LRUD_astar_sequence.append(current_parent.direction)
+                depth = self.compute_cost_g(current_parent)
+
+                while current_parent.parent_node:
+                    current_parent = current_parent.parent_node
+                    LRUD_astar_sequence.append(current_parent.direction)
+
+                LRUD_astar_sequence.remove(None)
+
+                LRUD_astar_sequence.reverse()
+                solution_length = len(LRUD_astar_sequence)
+
+                print(solution_length)
+                print(depth)
+                print(LRUD_astar_sequence)
+                print(visited_nodes_stats)
+                print(processed_nodes_stats)
+                print(exec_time)
+
+                print('Solution found.')
+
+
+                return LRUD_astar_sequence, solution_length, depth, exec_time,\
+                       visited_nodes_stats, processed_nodes_stats
 
             for direction in moves_sequence:
                 child = current_parent.create_one_child(direction)
@@ -76,22 +108,30 @@ class Astar:
                 if child is not None:
                     # if child.board in visited_boards:
                     # if child.board in seen:
-                    if self.state(child) in seen:
+                    if child.board in visited_boards:
                         visited_nodes_stats += 1
 
                     else:
                         child.distance = self.calculate_f_value(child)
-                        parents_pool.appendleft(child)
+                        parents_pool.append(child)
                         # seen.add(self.state(child))
                         # visited_boards.append(child.board)
 
-            # visited_boards.append(current_parent.board)
-            seen.add(self.state(current_parent))
-
+            visited_boards.append(current_parent.board)
 
             processed_nodes_stats += 1
             visited_nodes_stats += 1
 
-        print('No solution found')
+            if len(parents_pool) == 0:
+                depth_last = self.calculate_f_value(current_parent)
 
-        return None
+
+        end_time = datetime.datetime.now()
+        exec_time = (end_time - start_time).total_seconds() * 1000
+
+        solution_length = -1
+        LRUD_solution_sequence = None
+
+        print('No solution found.')
+
+        return LRUD_astar_sequence, solution_length, depth_last, exec_time, visited_nodes_stats, processed_nodes_stats
